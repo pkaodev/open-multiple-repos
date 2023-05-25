ca7ff4e5d9c7d329e4f87f614e08dc78ff702f2c
pkaodev <polsson.development@gmail.com>
2023-05-25 10:07:56 +0100

    dev option

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import open from 'open'
 
+// 0. want to see dev layout on github?
+const devLayout = false
+
 // 1. set repo to open
 const repo = 'fun-katas'
 
@@ -10,19 +13,14 @@ https://github.com/Person2
 `
 
 // 3. run file
-// node index.js
-doTheThing(repo, githubs)
-
-function doTheThing(repo, githubs) {
-    githubs.trim().split('\n').forEach(github => {
-        try {
-            const user = github.split('.com/')[1]
-            const url = `https://github.com/${user}/${repo}`
-            open(url)
-            console.log(`Opened ${url}`)
-        } catch (e) {
-            console.log(`Error: ${github}
+githubs.trim().split('\n').forEach(github => {
+    try {
+        const user = github.split('.com/')[1]
+        const url = `https://github.${devLayout ? 'dev' : 'com'}/${user}/${repo}`
+        open(url)
+        console.log(`Opened ${url}`)
+    } catch (e) {
+        console.log(`Error: ${github}
             ${e}`)
-        }
-    })
-}
+    }
+})
